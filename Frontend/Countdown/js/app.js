c506d69344af27d1c3adee6f0bf322ffb9e44257
heisenberg-meth <nmdevi63@gmail.com>
2025-09-13 19:13:59 +0530

    19-09-2025 Para Generator

--- Frontend/Countdown/js/app.js
+++ Frontend/Countdown/js/app.js
@@ -1,5 +1,3 @@
-const { Component } = require("react");
-
 const months = [
   "January",
   "February",
@@ -27,47 +25,47 @@ const weekdays = [
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
-let tempDate = new Date();
-let tempYear = tempDate.getFullYear();
-let tempMonth = tempDate.getMonth();
-let tempDay = tempDate.getDate();
-const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
-const year = futureDate.getFullYear();
-const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes();
+
+let futureDate = new Date(2025, 9, 8, 0, 0, 0);
+const years = futureDate.getFullYear();
 let month = futureDate.getMonth();
 month = months[month];
 const weekday = weekdays[futureDate.getDay()];
+const days = futureDate.getDay();
 const date = futureDate.getDate();
-giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+giveaway.textContent = `Giveaway Ends on ${weekday} ${date} ${month} ${years} ${hours} ${minutes} am`;
 const futureTime = futureDate.getTime();
+
 function getRemaindingTime() {
   const today = new Date().getTime();
-
   const t = futureTime - today;
+  console.log(t);
   const oneDay = 24 * 60 * 60 * 1000;
   const oneHour = 60 * 60 * 1000;
   const oneMinute = 60 * 1000;
-  let days = t / oneDay;
-  days = Math.floor(days);
+  let days = Math.floor(t / oneDay);
   let hours = Math.floor((t % oneDay) / oneHour);
   let minutes = Math.floor((t % oneHour) / oneMinute);
   let seconds = Math.floor((t % oneMinute) / 1000);
 
   const values = [days, hours, minutes, seconds];
+
   function format(item) {
     if (item < 10) {
-      return (item = `0${item}`);
+      return `0${item}`;
+    } else {
+      return item;
     }
-    return item;
   }
-  items.forEach(function (item, index) {
+  items.forEach((item, index) => {
     item.innerHTML = format(values[index]);
   });
   if (t < 0) {
     clearInterval(countdown);
-    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    deadline.innerHTML = `<h3 class="expired">This is expired Unaku venum na ne sekram vanthu vaangirukanum da funda</h3>`;
   }
 }
 let countdown = setInterval(getRemaindingTime, 1000);
-getRemaindingTime();
