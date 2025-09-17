be0d97240a2ff991dba1b821b12e933385958216
heisenberg-meth <nmdevi63@gmail.com>
2025-09-17 09:24:36 +0530

    16-09-2025

--- Frontend/DrumsKit/js/app.js
+++ Frontend/DrumsKit/js/app.js
@@ -1,20 +1,3 @@
-// function playSound(e) {
-//   const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-//   const key = document.querySelector(`.key[data-key="${e.keycode}"]`);
-//   if (!audio) return;
-//   audio.currentTime = 0;
-//   audio.play();
-//   key.classList.add("playing");
-// }
-// function removeTransition(e) {
-//   if (e.propertyName !== "transform") return;
-//   this.classList.remove("playing");
-// }
-// const keys = document.querySelectorAll(".key");
-// keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
-// window.addEventListener("keydown", playSound);
-
-
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
