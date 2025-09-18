826fb21c2280ac6b6bb862b9801519005cf818eb
heisenberg-meth <nmdevi63@gmail.com>
2025-09-18 15:39:05 +0530

    19-09-2025 Task 2 Completed

---
+++ Frontend Tasks/Scroll/js/app.js
@@ -0,0 +1,9 @@
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
