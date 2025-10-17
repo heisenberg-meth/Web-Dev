511f916a8436b3685242e270ea1d73aca5bb0fd0
heisenberg-meth <nmdevi63@gmail.com>
2025-10-17 18:02:17 +0530

    juno mail added

--- Juno Naturals/js/app.js
+++ Juno Naturals/js/app.js
@@ -11,3 +11,48 @@ buttons.forEach((btn) => {
     if (selected) selected.classList.add("active");
   });
 });
+
+const forms = document.querySelectorAll(".needs-validation");
+
+forms.forEach(function (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (form.checkValidity()) {
+      sendEmail(form);
+    }
+    form.classList.add("was-validated");
+  });
+});
+
+function sendEmail(form) {
+  const params = {
+    name: document.getElementById("name").value.trim(),
+    age: document.getElementById("age").value.trim(),
+    gender: document.getElementById("gender").value.trim(),
+    mobile: document.getElementById("mobile").value.trim(),
+    whatsappnumber: document.getElementById("wnumber").value.trim(),
+    email: document.getElementById("email").value.trim(),
+    address: document.getElementById("address").value.trim(),
+    course: document.getElementById("course").value.trim(),
+    message: document.getElementById("message").value.trim(),
+  };
+
+  const serviceID = "service_4a4u7pr";
+  const templateID = "template_0szhajf";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((response) => {
+      console.log("SUCCESS!", response.status, response.text);
+      alert(
+        "Your Message Will Be sent To Dynamic Electrocplaters Private Limited!"
+      );
+      form.reset();
+      form.classList.remove("was-validated");
+    })
+    .catch((error) => {
+      console.error("FAILED...", error);
+      alert("❌ Message failed to send. Try again!");
+    });
+}
